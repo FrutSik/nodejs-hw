be405a803469397604ed64d86d3f0885d98e9942
FrutSik <makapov172007@gmail.com>
2025-12-13 11:40:11 +0200

    Fix

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+  },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+    },
+  },
+];
