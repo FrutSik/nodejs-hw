861b3fa3a6a2f1b233f8d8c991bf44a43745b96d
FrutSik <makapov172007@gmail.com>
2025-12-24 23:31:26 +0200

    Fix

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,15 +1,9 @@
 import express from 'express';
 import { updateUserAvatar } from '../controllers/userController.js';
 import { authenticate } from '../middleware/authenticate.js';
-import { upload } from '../middleware/multer.js';
 
 const router = express.Router();
 
-router.patch(
-  '/users/me/avatar',
-  authenticate,
-  upload.single('avatar'),
-  updateUserAvatar,
-);
+router.patch('/users/me/avatar', authenticate, updateUserAvatar);
 
 export default router;
